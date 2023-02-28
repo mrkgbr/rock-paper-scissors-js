b4d19567a5af2d3db6a0a43b3be501a65a178803
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-02-28 15:11:01 +0100

    Update game function, added comments
    Added user input validation, keeps asking if chice is invalid
    Added comments for better understanding

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
+// 1.
 // Create a function named getComputerChoice
 // This function returns rock, paper or scissors randomly when the function gets called
 // Crate an array
 // Use Math functions to generate a random number to get the item from the array
-
-const listOfChoices = ["rock", "paper", "scissors"];
-
-function getComputerChoice() {
-  return listOfChoices[Math.floor(Math.random() * listOfChoices.length)];
-}
-
-// Create a play function
+// 2.
 // The function should take two parameters: playerSelection and computerSelection
 // Return a string that declares the winner of the round
 // The input function should be case-insensitive
 // Use if or switch to compare choices
+
+// Variables
+const listOfChoices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let playerSelection = "";
+let computerSelection = "";
+
+// Generate a random number then multiply it with the length of the list then round it to the floor
+// If the list length is 3, then it will generate 1, 2 or 3.
+function getComputerChoice() {
+  return listOfChoices[Math.floor(Math.random() * listOfChoices.length)];
+}
+
+// Compares the given choices and gives back the result
 // if the choices are equal, then it's a tie
 // rock beats scissors
 // scissors beats paper
 // paper beats rock
+// keeps track the user score
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "It's a tie!";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    playerScore++
+    playerScore++;
     return "You Won! Rock beats Scissors!";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerScore++
+    playerScore++;
     return "You Won! Scissors beats Paper!";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerScore++
+    playerScore++;
     return "You Won! Paper beats Rock!";
   } else {
     computerSelection =
@@ -39,16 +48,25 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-let playerScore = 0
-
+// 5 round game
+// Asks user choice
+// Input validation
+// Generates computer choice
 function game() {
   for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Please enter your choice: ").toLowerCase();
-    let computerSelection = getComputerChoice();
+    while (true) {
+      playerSelection = prompt("Please enter your choice: ").toLowerCase();
+      if (listOfChoices.includes(playerSelection)) {
+        break;
+      } else {
+        console.log("Wrong answer. Try it again. Rock, Paper or Scissor");
+      }
+    }
+    computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
-    console.log(`Your score is ${playerScore} after round ${i + 1}`)
+    console.log(`Your score is ${playerScore} after round ${i + 1}`);
   }
-  console.log(`The Game is Over. Your final score is ${playerScore}`)
+  console.log(`The Game is Over. Your final score is ${playerScore}`);
 }
 
-game()
+game();
