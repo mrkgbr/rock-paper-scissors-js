45587a41bd0b44fb6d3189fe9c111fe75119ea33
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-02-28 09:43:05 +0100

    Comlete playRound function

--- script.js
+++ script.js
@@ -13,12 +13,29 @@ function getComputerChoice() {
 // The function should take two parameters: playerSelection and computerSelection
 // Return a string that declares the winner of the round
 // The input function should be case-insensitive
+// Use if or switch to compare choices
+// if the choices are equal, then it's a tie
+// rock beats scissors
+// scissors beats paper
+// paper beats rock
 function playRound(playerSelection, computerSelection) {
-  if(playerSelection === computerSelection) {
-    return "It's a tie!"
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Won! Rock beats Scissors!";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Won! Scissors beats Paper!";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Won! Paper beats Rock!";
+  } else {
+    computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1)
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1)
+    return `You Lose! ${computerSelection} beats ${playerSelection}`
   }
 }
 
 const playerSelection = prompt("Please enter your choice: ").toLowerCase();
 const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
