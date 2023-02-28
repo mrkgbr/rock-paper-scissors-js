346329e2be34859099b164321a9cf54391524a1e
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-02-28 09:04:07 +0100

    Add description and playRound function

--- script.js
+++ script.js
@@ -3,8 +3,20 @@
 // Crate an array
 // Use Math functions to generate a random number to get the item from the array
 
-listOfChoices = ["Rock", "Paper", "Scissors"];
+const listOfChoices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
   return listOfChoices[Math.floor(Math.random() * listOfChoices.length)];
 }
+
+// Create a play function
+// The function should take two parameters: playerSelection and computerSelection
+// Return a string that declares the winner of the round
+// The input function should be case-insensitive
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
