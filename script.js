6e47de349608fb8561cc81a00539815f649a2951
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-02-28 10:32:47 +0100

    Completed basic program, all funnction works

--- script.js
+++ script.js
@@ -22,20 +22,33 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "It's a tie!";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++
     return "You Won! Rock beats Scissors!";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++
     return "You Won! Scissors beats Paper!";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++
     return "You Won! Paper beats Rock!";
   } else {
-    computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1)
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1)
-    return `You Lose! ${computerSelection} beats ${playerSelection}`
+    computerSelection =
+      computerSelection[0].toUpperCase() + computerSelection.slice(1);
+    playerSelection =
+      playerSelection[0].toUpperCase() + playerSelection.slice(1);
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
 
-const playerSelection = prompt("Please enter your choice: ").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+let playerScore = 0
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Please enter your choice: ").toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`Your score is ${playerScore} after round ${i + 1}`)
+  }
+  console.log(`The Game is Over. Your final score is ${playerScore}`)
+}
+
+game()
